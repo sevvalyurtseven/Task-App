f2be183800560d1a578e694f5134a1cd65b58cf7
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-20 18:57:24 +0300

    bir switch yapısının içerisinde sayfaların pathlerini tanimladık

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,7 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+import Login from "../pages/Login";
+import Tasks from "../pages/Tasks";
+import UserList from "../pages/UserList";
+import UserPage from "../pages/UserPage";
 const Main = () => {
   return (
     <div>
-      <div className="main-container">Main Area</div>
+      <div className="main-container">
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/tasks">
+            <Tasks />
+          </Route>
+          <Route exact path="/users">
+            <UserList />
+          </Route>
+          <Route exact path="/users/:id/:name/:surname">
+            <UserPage />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
