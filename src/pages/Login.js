af7646a58f5ea83545b6cca47fafcfcdb8b01958
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-20 19:46:06 +0300

    login form template

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,27 @@
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 const Login = () => {
   return (
-    <div className="login-container">
-      <h1>Login Area</h1>
-    </div>
+    <Form>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="with a placeholder"
+          type="email"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="password placeholder"
+          type="password"
+        />
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
   );
 };
 export default Login;
