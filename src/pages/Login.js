c14c918d387c05ba24191c1842b563c18e680301
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-20 19:58:53 +0300

    login form styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,33 @@
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import "./Login.css";
 const Login = () => {
   return (
-    <Form>
-      <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input
-          id="exampleEmail"
-          name="email"
-          placeholder="with a placeholder"
-          type="email"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input
-          id="examplePassword"
-          name="password"
-          placeholder="password placeholder"
-          type="password"
-        />
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
+    <>
+      <h2>LOGIN</h2>
+      <Form className="login-form">
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input
+            id="exampleEmail"
+            name="email"
+            placeholder="Please enter your email"
+            type="email"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input
+            id="examplePassword"
+            name="password"
+            placeholder="Please enter your password"
+            type="password"
+          />
+        </FormGroup>
+        <Button id="login-btn" color="primary">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 };
 export default Login;
