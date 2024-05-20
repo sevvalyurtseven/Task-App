edda0281cd03f0dda6b71c426acdd5afc64b0e70
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-20 19:02:12 +0300

    navlink ile sayfa yönlendirmeleri yapıldı ve css ile stil verildi

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,3 +1,6 @@
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
 const Header = (props) => {
   //destructure
   const { user } = props;
@@ -5,6 +8,15 @@ const Header = (props) => {
     <div>
       <header>
         <h1>TASK APP</h1>
+        <nav>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/tasks">Tasks</NavLink>
+          <NavLink to="/users">Users</NavLink>
+        </nav>
+        <div className="user-container">
+          <span className="profile-image"></span>
+          <span> {user.name}</span>
+        </div>
       </header>
     </div>
   );
