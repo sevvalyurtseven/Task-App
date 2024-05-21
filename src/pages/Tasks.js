65b6f5f9fe184617ee3a394ea095bc0e5ac9e790
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-21 16:52:22 +0300

    tasks template

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,7 +1,15 @@
-const Tasks = () => {
+import TaskList from "../components/TaskList";
+import AddTask from "../components/AddTask";
+import "./Tasks.css";
+
+const Tasks = (props) => {
+  //destructure
+  const { tasks, allUsers } = props;
   return (
     <div className="tasks-container">
-      <h1>Tasks Area</h1>
+      <AddTask allUsers={allUsers} />
+      <TaskList title="Active Tasks" type="incompleted" tasks={tasks} />
+      <TaskList title="Completed Tasks" type="completed" tasks={tasks} />
     </div>
   );
 };
