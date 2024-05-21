16f8d92809df1f7b605658ef1bdca74e6cf2efb9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-21 20:49:19 +0300

    status'e göre filtreleme yapacak yardımcı fonksiyon

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,7 @@
 export const fullName = (user) => {
   return `${user.first_name} ${user.last_name}`;
 };
+
+export const getTasksByStatus = (tasks, status) => {
+  return tasks.filter((task) => task.status === status);
+};
