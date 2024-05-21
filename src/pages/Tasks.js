639a6f2d8bca4df8d5c6621d3a01a4e6517866fd
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-21 18:35:12 +0300

    addTask fonksiyonu AddTask componentine iletildi

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -4,10 +4,10 @@ import "./Tasks.css";
 
 const Tasks = (props) => {
   //destructure
-  const { tasks, allUsers } = props;
+  const { tasks, allUsers, addTask } = props;
   return (
     <div className="tasks-container">
-      <AddTask allUsers={allUsers} />
+      <AddTask allUsers={allUsers} addTask={addTask} />
       <TaskList title="Active Tasks" type="incompleted" tasks={tasks} />
       <TaskList title="Completed Tasks" type="completed" tasks={tasks} />
     </div>
