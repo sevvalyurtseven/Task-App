19ec85a7cb3a94725c8b297c002fd94cb824489f
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-21 18:37:20 +0300

    task componenti reactstrap ile stillendirildi

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,31 @@
-const Task = () => {
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardText,
+  CardTitle,
+  CardFooter,
+  Button,
+} from "reactstrap";
+
+const Task = (props) => {
+  const { task } = props;
+
   return (
     <div className="task-card">
-      <h1>Task Area</h1>
+      <Card className="my-2" style={{ width: "18rem" }}>
+        <CardHeader>{task.deadline}</CardHeader>
+        <CardBody>
+          <CardTitle tag="h5">{task.subject}</CardTitle>
+          <CardText>{task.description}</CardText>
+          <Button>Complete</Button>
+        </CardBody>
+        <CardFooter>
+          {task.assignees.map((item) => (
+            <span key={item.id}> {item}</span>
+          ))}
+        </CardFooter>
+      </Card>
     </div>
   );
 };
